14c34fe3f6e2466b5c8ffe6cf68399ed4b448a3a
danishsayys0719 <danish.sayyed0719@gmail.com>
2025-11-25 20:12:38 +0530

    Refactor bot.py to load configuration from JSON

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
+import json
 import telebot
 
-BOT_TOKEN = "8226369849:AAFcWM34yciiTCTAy-3q-F0sD6E5dsSBpsc"
-CHAT_ID = "https://t.me/Sniper0719_bot"
+# Load config.json
+with open("config.json") as f:
+    config = json.load(f)
 
-bot = telebot.TeleBot(BOT_TOKEN)
+BOT_TOKEN = config["8226369849:AAFcWM34yciiTCTAy-3q-F0sD6E5dsSBpsc"]
+CHAT_ID = config["8183172862"]
+
+bot = telebot.TeleBot(8226369849:AAFcWM34yciiTCTAy-3q-F0sD6E5dsSBpsc)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Bot is running!")
+    bot.send_message(message.8183172862, "Bot is running!")
 
 def send_signal(text):
-    bot.send_message(CHAT_ID, text)
+    bot.send_message(8183172862, text)
 
-bot.polling()
+bot.pollingf()
